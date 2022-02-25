38f23531eaf5976c45d09c1b6079d7f63bad08fc
Tripti_Agarwal <31624673+tripti-agarwal@users.noreply.github.com>
2022-02-25 12:41:02 -0700

    VGood and VBad java volatile

---
+++ Assignment4/VBad.java
@@ -0,0 +1,54 @@
+public class VBad extends Thread{
+    int tid;
+    VBad(int i) { tid = i; 
+				System.out.println(i);}
+    static int N = 100;
+    static //volatile
+	boolean  req = false;
+    static volatile
+	boolean ack = false;    
+  public void run() {
+	int temp;
+	if (tid==0) {
+	    for (int i = 0; i < N; i++) {
+		req = true;
+		System.out.println(req);
+		while (ack==false) {
+		};
+		req = false;
+		while (ack==true) {
+		}
+	    }
+	}
+	else {
+	    for (int i = 0; i < N; i++) {
+		System.out.println(req);
+		while (req==false) {
+		}
+		ack = true;
+		while (req==true) {
+		}
+		ack = false;
+	    }
+	}
+    }
+    public static void main(String[] args) {
+	try {
+	    N = Integer.parseInt(args[0]); // User-supplied N
+	} 
+	catch (Exception e) {
+	} 
+	Thread t0 = new VBad(0);
+	Thread t1 = new VBad(1);
+	t0.start();
+	t1.start();
+	try {
+	    t0.join();
+	    t1.join();
+	}
+	catch (InterruptedException e) {
+	}
+	System.out.println("Done.");
+    }
+}
+
