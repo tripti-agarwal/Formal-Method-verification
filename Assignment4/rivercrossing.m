6437ccff16dc9fe4ab3b00a64aeecfc1f250afbb
Tripti_Agarwal <31624673+tripti-agarwal@users.noreply.github.com>
2022-02-25 11:40:23 -0700

    mwgc rumur code

---
+++ Assignment4/rivercrossing.m
@@ -0,0 +1,45 @@
+Var 
+    m, w, g, c : boolean;
+
+Function safe(m, w, g, c : boolean) : boolean;
+begin
+    return ((m = g) | ((w != g) & (g != c)));
+end;
+
+Rule "M and G move safely"
+    safe(!m, w, !g, c) & (m = g)
+==>
+    m := !m;
+    g := !g;   
+Endrule;
+
+Rule "M and W move safely"
+    safe(!m, !w, g, c) & (m = w)
+==>
+    m := !m;
+    w := !w;   
+Endrule;
+
+Rule "M and C move safely"
+    safe(!m, w, g, !c) & (m = c)
+==>
+    m := !m;
+    c := !c;   
+Endrule;
+
+
+Rule "M move safely"
+    safe(!m, w, g, c)
+==>
+    m := !m;  
+Endrule;
+
+Invariant 
+    !(m & w & g & c);
+    
+Startstate
+    m := false;
+    w := false;
+    g := false;
+    c := false;
+End;
