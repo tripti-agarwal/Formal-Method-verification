4a49d3c1dc2f9f39061fcf408d1a6848b8b762b1
Tripti_Agarwal <31624673+tripti-agarwal@users.noreply.github.com>
2022-05-02 16:28:22 -0600

    klee code

---
+++ Project/quickSort.c
@@ -0,0 +1,61 @@
+#include "klee/klee.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
+
+int partition(int *array, int low, int high) {
+  
+  int pivot = array[high];
+  
+  int i = (low - 1);
+
+  for (int j = low; j < high; j++) {
+    if (array[j] <= pivot) {
+      i++;
+      swap(&array[i], &array[j]);
+    }
+  }
+  swap(&array[i + 1], &array[high]);
+  return (i + 1);
+}
+
+void quickSort(int *array, int low, int high) {
+  if (low < high) {
+    int pi = partition(array, low, high);
+    quickSort(array, low, pi - 1);
+    quickSort(array, pi + 1, high);
+  }
+}
+
+
+
+void test(int *array, unsigned nelem) {
+  int *temp1 = malloc(sizeof(*array) * nelem);
+
+  memcpy(temp1, array, sizeof(*array) * 4);
+  
+
+  quickSort(temp1,0, 3);
+  for (unsigned i = 0; i != (nelem-1); ++i)
+    assert(temp1[i] <= temp1[i+1]);
+  free(temp1);
+  
+}
+
+int main() {
+  int input[4] = { 4, 3, 2, 1};
+
+  klee_make_symbolic(&input, sizeof(input), "input");
+  test(input, 4);
+
+  return 0;
+}
